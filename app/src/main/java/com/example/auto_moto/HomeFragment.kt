a1e517e8b4ac7cfbb7fdfe20f7d120bd0c89ecfa
David Aldrin Mondero <aldrinmondero09@gmail.com>
2023-10-19 23:16:19 +0800

    Added Forgot and Confirmation fragment, Also fixed recyclerview.

--- app/src/main/java/com/example/auto_moto/HomeFragment.kt
+++ app/src/main/java/com/example/auto_moto/HomeFragment.kt
@@ -8,6 +8,16 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class HomeFragment : Fragment() {
     private lateinit var adapter: Adapter
     private var recyclerView: RecyclerView? = null
@@ -25,7 +35,6 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
@@ -38,7 +47,6 @@ class HomeFragment : Fragment() {
         recyclerView?.setHasFixedSize(true)
         adapter = Adapter(carArray)
         recyclerView?.adapter = adapter
-
     }
     private fun prepareCarList(){
         carArray = arrayListOf<CarItems>()
