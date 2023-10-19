f3115700da08cbf189f3c78b8363b2daa86f302e
Mark Benedict Abalos <benedictabalos30@gmail.com>
2023-10-20 00:23:53 +0800

    Add somethings

--- app/src/main/java/com/example/auto_moto/LoginFragment.kt
+++ app/src/main/java/com/example/auto_moto/LoginFragment.kt
@@ -7,17 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.auto_moto.databinding.FragmentLoginBinding
+import com.example.auto_motov04.LoginFragmentDirections
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
     override fun onCreateView(
