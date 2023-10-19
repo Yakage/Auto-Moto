bf1b6a4a85bfac4de6c201d4dce121d13bb9053c
Mark Benedict Abalos <benedictabalos30@gmail.com>
2023-10-20 00:02:28 +0800

    Add confirmationFragment

---
+++ app/src/main/java/com/example/auto_moto/ConfirmationFragment.kt
@@ -0,0 +1,26 @@
+package com.example.auto_moto
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.auto_moto.databinding.FragmentConfirmationBinding
+
+class ConfirmationFragment : Fragment() {
+
+    private lateinit var binding: FragmentConfirmationBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentConfirmationBinding.inflate(inflater, container, false)
+        binding.btConfirmation.setOnClickListener {
+            findNavController().navigate(ConfirmationFragmentDirections.actionConfirmationFragmentToReceiptFragment())
+        }
+        return binding.root
+    }
+
+
+}
