803d5eebd2de6c27cc9c8a0fbdc5ac85ef8856f7
Mark Benedict Abalos <benedictabalos30@gmail.com>
2023-10-22 23:15:47 +0800

    add some background for drawer

---
+++ app/src/main/java/com/example/auto_moto/BodyAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.auto_moto
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class BodyAdapter {
+}
---
+++ app/src/main/java/com/example/auto_moto/TiresAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.auto_moto
+
+class TiresAdapter {
+}
