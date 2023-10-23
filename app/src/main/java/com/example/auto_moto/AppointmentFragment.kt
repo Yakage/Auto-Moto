92b648b595869082945dad2c2ad9fb95f29bbc8b
Mark Benedict Abalos <benedictabalos30@gmail.com>
2023-10-23 09:39:26 +0800

    Add database for appointments

--- app/src/main/java/com/example/auto_moto/AppointmentFragment.kt
+++ app/src/main/java/com/example/auto_moto/AppointmentFragment.kt
@@ -5,16 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.auto_moto.databinding.FragmentAppointmentBinding
 
 
 class AppointmentFragment : Fragment() {
     private lateinit var binding: FragmentAppointmentBinding
+    private lateinit var databaseHelper: DatabaseHelperForAppointment
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentAppointmentBinding.inflate(inflater, container, false)
@@ -26,6 +30,27 @@ class AppointmentFragment : Fragment() {
         }
         binding.btAddCar.setOnClickListener {
             findNavController().navigate(AppointmentFragmentDirections.actionAppointmentFragmentToAddNewCarsFragment())
+        }
+
+        databaseHelper = DatabaseHelperForAppointment(requireContext())
+
+        binding.btContinue.setOnClickListener {
+            // Get the appointment data from your UI (services, car type, date, time, service charge)
+            val services = "Car Wash" // Replace with actual data
+            val carType = "Mercedes Benz" // Replace with actual data
+            val date = "Fri Oct 20, 2023" // Replace with actual data
+            val time = "05 to 06 PM" // Replace with actual data
+            val serviceCharge = 20000.0 // Replace with actual data
+
+            // Create an Appointment object with the data
+            val appointment = DatabaseHelperForAppointment.AppointmentData(services, carType, date, time, serviceCharge)
+
+            // Insert the appointment into the database
+            databaseHelper.insertAppointment(appointment)
+
+            // Optionally, you can show a confirmation message or perform any other actions
+            Toast.makeText(requireContext(), "Appointment added successfully", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(AppointmentFragmentDirections.actionAppointmentFragmentToConfirmationFragment())
         }
         return binding.root
     }
--- app/src/main/java/com/example/auto_moto/ConfirmationFragment.kt
+++ app/src/main/java/com/example/auto_moto/ConfirmationFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.example.auto_moto.databinding.FragmentConfirmationBinding
 
 class ConfirmationFragment : Fragment() {
     private lateinit var binding: FragmentConfirmationBinding
+    private lateinit var databaseHelper: DatabaseHelperForAppointment
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,13 +20,42 @@ class ConfirmationFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentConfirmationBinding.inflate(inflater, container, false)
 
-        val receivedData = arguments?.getString("price")
-        binding.tvTypesOfServices.text = receivedData
 
         binding.ibBackArrow.setOnClickListener {
             findNavController().navigate(ConfirmationFragmentDirections.actionConfirmationFragmentToAppointmentFragment())
         }
         binding.btPayNow.setOnClickListener {
+            findNavController().navigate(ConfirmationFragmentDirections.actionConfirmationFragmentToReceiptFragment())
+        }
+
+        // Initialize the DatabaseHelper
+        databaseHelper = DatabaseHelperForAppointment(requireContext())
+
+
+        // Retrieve data from the database (replace with your database schema)
+
+        val appointmentData = databaseHelper.getAppointmentById(0)
+
+        // Populate TextViews with retrieved data
+        if (appointmentData != null) {
+            binding.tvTypesOfServices.text = appointmentData.services
+        }
+        if (appointmentData != null) {
+            binding.tvMyCar.text = appointmentData.carType
+        }
+        if (appointmentData != null) {
+            binding.tvDate.text = appointmentData.date
+        }
+        if (appointmentData != null) {
+            binding.tvTime.text = appointmentData.time
+        }
+        if (appointmentData != null) {
+            binding.serviceCharge.text = "PHP ${appointmentData.serviceCharge}"
+        }
+
+        // Handle the "Pay Now" button click event if needed
+        binding.btPayNow.setOnClickListener {
+            // Implement your payment logic here
             findNavController().navigate(ConfirmationFragmentDirections.actionConfirmationFragmentToReceiptFragment())
         }
 
